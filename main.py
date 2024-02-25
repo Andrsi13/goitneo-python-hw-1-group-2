fe3775057e6be192fca5e7e10ee41dfeedf40a92
Andrsi13 <vladika.andrsi@gmail.com>
2024-02-25 23:25:59 +0200

    update

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-users = {"name": "Bill Gates", "birthday": datetime(1955, 10, 28)}
+from datetime import datetime
+from collections import defaultdict
+
+
+def get_birthdays_per_week(users):
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Next Week Monday', 'Next Week Monday'] 
+    d = defaultdict(list)
+    d = {'Monday': [], 'Tuesday': [], 'Wednesday': [], 'Thursday': [], 'Friday': [], 'Next Week Monday': []}
+
+    today = datetime.today().date()
+    for user in users:
+        name = user["name"]
+        birthday = user["birthday"].date()  # Конвертуємо до типу date*
+        birthday_this_year = birthday.replace(year=today.year)
+        if birthday_this_year < today:
+            next_year_bd = birthday_this_year.replace(year=today.year + 1)
+        else:
+            delta_days = (birthday_this_year - today).days
+            if delta_days < 7:
+                weekday = birthday_this_year.weekday()
+            else:
+                continue
+        result = []
+        d[days[weekday]].append(name)
+        
+        for key_day, value_name in d.items():
+            if value_name:
+                result.append(f'{key_day}: {', '.join(value_name)}')
+                
+
+    return print('\n'.join(result))    #список користувачів у кого дн на цьому тижні
+
+
+a = [{"name": "Bill Gates", "birthday": datetime(1955, 2, 28)},{"name": "Papa Johnes", "birthday": datetime(1995, 2, 27)}, {"name": "Bereza Ivanivna", "birthday": datetime(1995, 3, 1)} ]  #список словників
+
+#print(get_birthdays_per_week(a))
+get_birthdays_per_week(a)
